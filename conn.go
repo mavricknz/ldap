0b0918663bc930c73070a8a94f3b7e8a39351fb3
Doron Sharon <dorsha123@gmail.com>
2018-12-16 18:57:08 +0200

    Protect go routine

--- conn.go
+++ conn.go
@@ -8,7 +8,8 @@ package ldap
 import (
 	"crypto/tls"
 	"fmt"
-	"github.com/demisto/server/util"
+	log "github.com/Sirupsen/logrus"
+	stackerr "github.com/go-errors/errors"
 	"github.com/mavricknz/asn1-ber"
 	"net"
 	"os"
@@ -157,10 +158,10 @@ func NewLDAPSSLConnection(server string, port uint16, tlsConfig *tls.Config) *LD
 }
 
 func (l *LDAPConnection) start() {
-	util.Go(func() {
+	Go(func() {
 		l.reader()
 	})
-	util.Go(func() {
+	Go(func() {
 		l.processMessages()
 	})
 }
@@ -413,5 +414,21 @@ func (l *LDAPConnection) sendProcessMessage(message *messagePacket) {
 		if l.connected {
 			l.chanProcessMessage <- message
 		}
+	}()
+}
+
+// RecoverWithStack is the defer function to recover from panic and print stack
+func RecoverWithStack() {
+	if err := recover(); err != nil {
+		log.Error(err)
+		log.Error(stackerr.Wrap(err, 2).ErrorStack())
+	}
+}
+
+// Go - run a function on a go routine with defer that prints logs
+func Go(f func()) {
+	go func() {
+		defer RecoverWithStack()
+		f()
 	}()
 }
